a5386f56255951b629f9e587d4d300dad4ed82cc
Madina <nishanbaeva.m.s.95@gmail.com>
2021-08-19 15:57:21 +0500

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    n = String(n);
+    var arr = n.split('');
+    arr = arr.reverse();
+    if (arr[arr.length - 1] === '-') { arr.pop(); }
+    return(arr.join(''));
 }
